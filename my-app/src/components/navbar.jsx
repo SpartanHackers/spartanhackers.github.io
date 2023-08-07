a39083cd276be5add729c497d83e664830a667e9
Rajmeet Singh Chandok <chandok1@msu.edu>
2023-08-08 05:15:48 +0530

    Created a react new app

---
+++ my-app/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <p>Navbar content</p>
+        </div>
+    );
+}
+
+export default Navbar;
